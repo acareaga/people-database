a5aee021e2ae64be8ccdcea760aa90424a52efc8
acareaga <aaroncareaga@gmail.com>
2016-02-16 08:45:19 -0700

    Init repo and started people class

---
+++ people_database.rb
@@ -0,0 +1,7 @@
+# PersonDatabase should be initialized with an array of Person objects
+
+# people = [...]
+# people_db = PeopleDatabase.new(people)
+
+# Create a method
+# people_db.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "CO")
---
+++ person.rb
@@ -0,0 +1,1 @@
+# Person should have the attributes of a first name, last name, email, and state.
