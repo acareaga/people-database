06c5c4b11d5bd41ce9dd31c58dcec352a08ab40c
acareaga <aaroncareaga@gmail.com>
2016-02-17 08:37:40 -0700

    Building out person db to create a new person record

--- people_database.rb
+++ people_database.rb
@@ -3,7 +3,8 @@
 # people = [...]
 # people_db = PeopleDatabase.new(people)
 
-require ('./person')
+require './person'
+require 'pry'
 
 class PersonDatabase
 
@@ -12,6 +13,7 @@ class PersonDatabase
   end
 
   def self.add
+    Person.new()
     # people_db.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "CO")
   end
 end
