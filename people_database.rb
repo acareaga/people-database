6746773f8447254f1713afd304ce58c20080ea6e
acareaga <aaroncareaga@gmail.com>
2016-02-17 08:41:40 -0700

    Person attr almost working on db

--- people_database.rb
+++ people_database.rb
@@ -6,14 +6,15 @@
 require './person'
 require 'pry'
 
-class PersonDatabase
+class PeopleDatabase
 
   def initialize
     @person = Person.new
   end
 
-  def self.add
-    Person.new()
-    # people_db.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "CO")
+  def self.add(attributes)
+    Person.new(attributes)
   end
 end
+
+people_db = PeopleDatabase.add(first_name: "Tess", last_name: "Griffin", email: "tess@turing.io", state: "CO")
--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 # Person should have the attributes of a first name, last name, email, and state.
 
 class Person
-  def intialize
+  def intialize(attributes)
     @first_name = first_name
     @last_name = last_name
     @email = email
